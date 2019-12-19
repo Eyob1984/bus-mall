0cb202899ebcf6c9ee1e4731c4d2a46a318666d9
Eyob1984 <tamireyob@gmail.com>
2019-12-18 16:01:35 -0800

    local storage created and working fine

--- app.js
+++ app.js
@@ -8,18 +8,57 @@ var voteIndex2 = 1;
 var voteIndex3 = 2;
 var allVotes = [];
 var totalClicks = 0;
-var clickCount = 0;
 
 
-function Voteimg(name, imageUrl){
+function Voteimg(name, imageUrl,timesClicked = 0,imageview = 0){
   this.name = name;
   this.imageUrl = imageUrl;
-  this.timesClicked = 0;
-  this.imageview = 0;
+  this.timesClicked = timesClicked;
+  this.imageview = imageview;
+
   allVotes.push(this);
 
 }
 
+var gettingImages = localStorage.getItem('newGetImage');
+if (gettingImages){
+  var changeToObject = JSON.parse(gettingImages);
+  for(let i=0; i<changeToObject.length; i++)
+    new Voteimg(changeToObject[i].name, changeToObject[i].imageUrl,changeToObject[i].timesClicked, changeToObject[i].imageview);
+}else{
+
+  new Voteimg('bag','img/bag.jpg');
+  new Voteimg('bnanana', 'img/banana.jpg');
+  new Voteimg('bathroom', 'img/bathroom.jpg');
+  new Voteimg('boots', 'img/boots.jpg');
+  new Voteimg('breakfast', 'img/breakfast.jpg');
+  new Voteimg('bubblegum', 'img/bubblegum.jpg');
+  new Voteimg('chair', 'img/chair.jpg');
+  new Voteimg('cthulhu', 'img/cthulhu.jpg');
+  new Voteimg('dog-duck','img/dog-duck.jpg');
+  new Voteimg('dragon', 'img/dragon.jpg');
+  new Voteimg('pen', 'img/pen.jpg');
+  new Voteimg('pet-sweep', 'img/pet-sweep.jpg');
+  new Voteimg('scissors', 'img/scissors.jpg');
+  new Voteimg('shark','img/shark.jpg');
+  new Voteimg('sweep', 'img/sweep.png');
+  new Voteimg('tauntaun', 'img/tauntaun.jpg');
+  new Voteimg('unicorn', 'img/unicorn.jpg');
+  new Voteimg('usb', 'img/usb.gif');
+  new Voteimg('water-can','img/water-can.jpg');
+  new Voteimg('wine-glass', 'img/wine-glass.jpg');
+
+}
+// code pulled from www.https://www.w3schools.com/
+function random_bg_color() {
+  var x = Math.floor(Math.random() * 256);
+  var y = Math.floor(Math.random() * 256);
+  var z = Math.floor(Math.random() * 256);
+  var bgColor = 'rgb(' + x + ',' + y + ',' + z + ')';
+  console.log(bgColor);
+
+  return bgColor;
+}
 function getTheName(){
   var getName = [];
   for (var b = 0; b < allVotes.length; b++){
@@ -42,38 +81,6 @@ function getImageViewed(){
   }
   return getimage;
 }
-function random_bg_color() {
-  var x = Math.floor(Math.random() * 256);
-  var y = Math.floor(Math.random() * 256);
-  var z = Math.floor(Math.random() * 256);
-  var bgColor = 'rgb(' + x + ',' + y + ',' + z + ')';
-  console.log(bgColor);
-
-  return bgColor;
-}
-
-
-new Voteimg('bag','img/bag.jpg');
-new Voteimg('bnanana', 'img/banana.jpg');
-new Voteimg('bathroom', 'img/bathroom.jpg');
-new Voteimg('boots', 'img/boots.jpg');
-new Voteimg('breakfast', 'img/breakfast.jpg');
-new Voteimg('bubblegum', 'img/bubblegum.jpg');
-new Voteimg('chair', 'img/chair.jpg');
-new Voteimg('cthulhu', 'img/cthulhu.jpg');
-new Voteimg('dog-duck','img/dog-duck.jpg');
-new Voteimg('dragon', 'img/dragon.jpg');
-new Voteimg('pen', 'img/pen.jpg');
-new Voteimg('pet-sweep', 'img/pet-sweep.jpg');
-new Voteimg('scissors', 'img/scissors.jpg');
-new Voteimg('shark','img/shark.jpg');
-new Voteimg('sweep', 'img/sweep.png');
-new Voteimg('tauntaun', 'img/tauntaun.jpg');
-new Voteimg('unicorn', 'img/unicorn.jpg');
-new Voteimg('usb', 'img/usb.gif');
-new Voteimg('water-can','img/water-can.jpg');
-new Voteimg('wine-glass', 'img/wine-glass.jpg');
-
 
 
 function imageClicked(event) {
@@ -116,16 +123,18 @@ function imageClicked(event) {
   imageElements[2].src = allVotes[voteIndex3].imageUrl, allVotes[voteIndex3].imageview++;
 
   console.log(allVotes[voteIndex1].imageview);
-  if(totalClicks >= 25) {
+  if(totalClicks >= 5) {
     for(var j = 0; j < imageElements.length; j++){
-      console.log(imageElements[j]);
+      // console.log(imageElements[j]);
       imageElements[j].removeEventListener('click', imageClicked);
-      creatList();
-
     }
+
+    creatList();
     magicJschart();
   }
+  localStorage.setItem('newGetImage',JSON.stringify(allVotes));
 }
+
 
 for (var i = 0; i < imageElements.length; i++) {
   imageElements[i].addEventListener('click', imageClicked);
@@ -156,14 +165,7 @@ function magicJschart(){
         label: '# of view',
         data: getImageViewed(),
         backgroundColor: random_bg_color(),
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
+        borderColor: random_bg_color(),
         borderWidth: 1
       }]
     },
